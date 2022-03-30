183ee34ad4e29389ba9d377d5f64544e0c248e82
Haifa <haifa.massoud@gmail.com>
2022-03-30 16:25:20 +0200

    add api link

--- src/params.js
+++ src/params.js
@@ -1,2 +1,2 @@
-export const API_URL = "http://localhost:8000";
-export const MEDIA_URL = "http://localhost:8000";
+export const API_URL = "https://dream-travel-agency.herokuapp.com/";
+export const MEDIA_URL = "https://dream-travel-agency.herokuapp.com/";
