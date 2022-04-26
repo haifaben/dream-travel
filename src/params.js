d865bd05b368f44507f985000261c1252ea9ecd6
haifaben <haifa.massoud@gmail.com>
2022-04-26 16:56:09 +0200

    Update params.js

--- src/params.js
+++ src/params.js
@@ -1,2 +1,2 @@
 export const API_URL = "https://dream-travel-agency.herokuapp.com/";
-export const MEDIA_URL = "https://dream-travel-agency.herokuapp.com/";
+export const MEDIA_URL = "https://git.heroku.com/dream-travel-agency.git/";
